57b1e48429c7964bb981d3bd9e9889b16eecfc7d
Dediluk <dediluk@gmail.com>
2019-12-26 10:27:56 +0300

    -Кодировка изменена на utf-8

--- bakup_ver1.py
+++ bakup_ver1.py
@@ -1,37 +1,35 @@
-# coding=windows-1251
-
 import os
 import time
 import zipfile
 
 target_dir = 'D:\\backups'
-backup_folder_name = target_dir + os.sep + time.strftime("%Y%m%d")      #��� ������ �� ���� ���� �������� � ����� �����
+backup_folder_name = target_dir + os.sep + time.strftime("%Y%m%d")      #все бэкапы за один день хранятся в одной папке
 
 if not os.path.exists(backup_folder_name):
     os.mkdir(backup_folder_name)
-    print("������� {0} ������� ������!".format(backup_folder_name))
+    print("Каталог {} успешно создан!".format(backup_folder_name))
 
 while True:
-    source = input("������� ����:")                                     #���� � �����/�����, ������� ���� ��������������
+    source = input("Введите путь:")                                     #путь до файла/папки, которые хотим заархивировать
 
     if not os.path.isdir(source):
-        print("������. ���������� �����!")
+        print("Данного каталога не существует! Попробуйте снова")
         continue
 
-    user_comment = input("������� ����������� � ������:")               #����������� � �������� �����
+    user_comment = input("Введите свой комментарий к архиву:")          #комментарий добавляется к названию архива
 
-    if user_comment == 0:                                               #���������� �����������, ���� �� ����
+    if user_comment == 0:                                               #добавление комментария, если он существует
         target = backup_folder_name + os.sep + time.strftime('%H%M') + '.zip'
     else:
         target = backup_folder_name + os.sep + time.strftime('%H%M') + "_" + user_comment.replace(" ", "_")  +'.zip'
 
-    try:                                                                #�������� ������ � ������ ������ � ����
+    try:                                                                #создание архива и запись файлов в него
         zip_backup = zipfile.ZipFile(target, 'w')
         time_before = time.time()
         for root, dirs, files in os.walk(source):
             for file in files:
                 zip_backup.write(os.path.join(root, file))
-        print("����� ������ � {0} �� {1} ���".format(backup_folder_name, round(time.time() - time_before, 3)))
+        print("Архив успешно создан в {0} за {1} сек".format(backup_folder_name, round(time.time() - time_before, 3)))
         break
     except:
-        print("������. ���������� �����!")
+        print("Ошибка! Попробуйте снова")
