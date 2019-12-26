6f4b7e6f4cb66b11545e487aa4771725f61888e9
Dediluk <dediluk@gmail.com>
2019-12-26 10:05:21 +0300

    -Изменена структура папок с бэкапами -Добавлена возможность добавление комментариев к архиву -Добавлено время создания архива -Добавлены комментарии

--- bakup_ver1.py
+++ bakup_ver1.py
@@ -1,23 +1,37 @@
+# coding=windows-1251
+
 import os
 import time
 import zipfile
 
 target_dir = 'D:\\backups'
+backup_folder_name = target_dir + os.sep + time.strftime("%Y%m%d")      #все бэкапы за один день хранятся в одной папке
+
+if not os.path.exists(backup_folder_name):
+    os.mkdir(backup_folder_name)
+    print("Каталог {0} успешно создан!".format(backup_folder_name))
 
 while True:
-    source = input("Input path:")
+    source = input("Введите путь:")                                     #путь к папке/файлу, которые надо заархивировать
+
     if not os.path.isdir(source):
-        print("Fail. Try again")
+        print("Ошибка. Попробуйте снова!")
         continue
 
-    target = target_dir + os.sep + time.strftime('%Y%m%d%H%M') + '.zip'
-    try:
-        zip_backup = zipfile.ZipFile(target, 'w')
+    user_comment = input("Введите комментарий к архиву:")               #комментарий к названию файла
 
+    if user_comment == 0:                                               #добавление комментария, если он есть
+        target = backup_folder_name + os.sep + time.strftime('%H%M') + '.zip'
+    else:
+        target = backup_folder_name + os.sep + time.strftime('%H%M') + "_" + user_comment.replace(" ", "_")  +'.zip'
+
+    try:                                                                #создание архива и запись файлов в него
+        zip_backup = zipfile.ZipFile(target, 'w')
+        time_before = time.time()
         for root, dirs, files in os.walk(source):
             for file in files:
                 zip_backup.write(os.path.join(root, file))
-        print("Zip is ready at ", target_dir)
+        print("Архив создан в {0} за {1} сек".format(backup_folder_name, round(time.time() - time_before, 3)))
         break
     except:
-        print("Failure. Try again")
+        print("Ошибка. Попробуйте снова!")
